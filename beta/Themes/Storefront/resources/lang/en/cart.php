6f7573f64dbccd1e918c40d7e2bbea1dd77fe3e3
souheibmadi <sm@aimngt.com>
2022-03-13 19:34:35 +0100

    first commit 2

---
+++ beta/Themes/Storefront/resources/lang/en/cart.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'cart' => 'Mon Panier',
+    'shopping_cart' => 'SHOPPING CART',
+    'checkout' => 'Payer',
+    'order_complete' => 'ORDER COMPLETE',
+    'table' => [
+        'image' => 'IMAGE',
+        'product_name' => 'Nom de Produit',
+        'unit_price' => 'UNIT PRICE',
+        'quantity' => 'QUANTITY',
+        'line_total' => 'LINE TOTAL',
+        'unit_price:' => 'Unit Price:',
+        'quantity:' => 'Quantity:',
+        'line_total:' => 'Line Total:',
+    ],
+    'enter_coupon_code' => 'Enter coupon code',
+    'apply_coupon' => 'APPLY COUPON',
+    'order_summary' => 'Order Summary',
+    'subtotal' => 'Subtotal',
+    'coupon' => 'Coupon',
+    'shipping_method' => 'livraison ',
+    'total' => 'Total',
+    'proceed_to_checkout' => 'PROCEED TO CHECKOUT',
+    'your_cart_is_empty' => 'Your cart is empty',
+    'looks_like_you_haven\'t_made_any_choice_yet' => 'Looks like you haven\'t made any choice yet.',
+];
---
+++ beta/Themes/Storefront/resources/lang/en/product_card.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'out_of_stock' => 'Out of Stock',
+    'new' => 'Nouveau',
+    'add_to_cart' => 'AJOUTER AU PANIER',
+    'view_options' => 'VIEW OPTIONS',
+    'compare' => 'Compare',
+    'wishlist' => 'Wishlist',
+    'available' => 'Available:',
+    'sold' => 'Sold:',
+    'years' => 'Years',
+    'months' => 'Months',
+    'weeks' => 'Weeks',
+    'days' => 'Days',
+    'hours' => 'Hours',
+    'minutes' => 'Minutes',
+    'seconds' => 'Seconds',
+];
